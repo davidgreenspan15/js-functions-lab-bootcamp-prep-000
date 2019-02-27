3c1e2ecd9c28cfc589462e9e25fd05d586ccf485
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-27 15:30:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday,name){
   return (`Happy ${holiday}, ${name}!`)
 }
-function holidayCountdown(holiday,days){
+function holidayCountdown(holiday, days){
   returns (`It's ${days} days until${holiday}!` )
 }
