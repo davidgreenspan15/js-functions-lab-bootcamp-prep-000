b2e06db78baccc430dbf3b5cf003bf66e66b81ad
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-27 15:13:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays(){
   return ("Happy holidays!")
 }
 function happyHolidaysTo(name){
-  return ("Happy holidays, ${name}!")
+  return "Happy holidays, ${name}!"
 }
 function happyHolidayto(holiday,name){
   return ("Happy ${holiday}, ${name}")
